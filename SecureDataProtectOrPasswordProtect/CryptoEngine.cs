d22d909824d839d55f6be6f3ecff3ccc5b0043d9
mdshohagmia <shohaghassan14@hotmail.com>
2019-01-18 15:14:45 +0600

    Added New encription

---
+++ SecureDataProtectOrPasswordProtect/CryptoEngine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SecureDataProtectOrPasswordProtect
+{
+    class CryptoEngine
+    {
+        public const string key = "sblw-3hn8-sqoy19";
+        public static string Encrypt(string input)
+        {
+            byte[] inputArray = Encoding.UTF8.GetBytes(input);
+            TripleDESCryptoServiceProvider tripleDes = new TripleDESCryptoServiceProvider
+            {
+                Key = Encoding.UTF8.GetBytes(key),
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+            ICryptoTransform cTransform = tripleDes.CreateEncryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(inputArray, 0, inputArray.Length);
+            tripleDes.Clear();
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+        public static string Decrypt(string input)
+        {
+            byte[] inputArray = Convert.FromBase64String(input);
+            TripleDESCryptoServiceProvider tripleDes = new TripleDESCryptoServiceProvider
+            {
+                Key = Encoding.UTF8.GetBytes(key),
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+            ICryptoTransform cTransform = tripleDes.CreateDecryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(inputArray, 0, inputArray.Length);
+            tripleDes.Clear();
+            return Encoding.UTF8.GetString(resultArray);
+        }
+    }
+}
--- SecureDataProtectOrPasswordProtect/Program.cs
+++ SecureDataProtectOrPasswordProtect/Program.cs
@@ -7,14 +7,28 @@ namespace SecureDataProtectOrPasswordProtect
     {
         static void Main(string[] args)
         {
+            string cryptoEnginee = "Shohag";
+            Console.WriteLine("Original Name- " + cryptoEnginee);
+            string encript = CryptoEngine.Encrypt(cryptoEnginee);
+
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Encrypt Text- " + encript);
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            string decript = CryptoEngine.Decrypt(encript);
+            Console.WriteLine("Decript Text- " + decript);
+
+
+            Console.WriteLine();
+
 
             string password = "123456";
-            Console.WriteLine("Real Password-" + password);
+            Console.WriteLine("Original Password-" + password);
 
             Console.BackgroundColor = ConsoleColor.DarkRed;
             SecureDataProtected aDataProtected = new SecureDataProtected();
             Console.WriteLine("Protected Data-" + aDataProtected.Protect(password));
-            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.BackgroundColor = ConsoleColor.Black;
 
             string original = aDataProtected.Unprotect(ConfigurationManager.AppSettings["password"]);
             Console.WriteLine("Unprotected Data-" + original);
